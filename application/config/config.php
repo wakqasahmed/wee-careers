9829fc60f1eb1a8282a5eb44440c00ed2f620f9f
Waqas Ahmed <wakqasahmed@gmail.com>
2014-10-01 16:01:45 +0400

    reverted config file

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.bazarstore.az/jobs/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
